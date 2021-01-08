600b0ace1f4509986a2fa8b4ae1d25c1e47ce05f
Jamila <jaja_mimi@hotmail.com>
2021-01-08 01:04:28 +0100

    exo foreach

---
+++ public/js/main.js
@@ -0,0 +1,16 @@
+// exo1
+let panier = ["pomme", "poire", "orange", "cerise", "orange"];
+
+panier.forEach(element => {
+    console.log(element);
+});
+
+
+console.log("______________");
+// exo 2
+
+let coding16 = ["saliou", "zakaria", "ayoub", "mohammed", "alex", "adame", "sebastien", "achraf", "bene", "abdellah", "malo", "louise", "natchez", "dawid", "maximilien", "jamila", "maxence", "zulma"];
+
+coding16.forEach(element => {
+    console.log(`bonjour ${element} tu es en position ${coding16.indexOf(element)}`);
+});
